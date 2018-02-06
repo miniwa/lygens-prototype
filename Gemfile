c9c381a902de3eb4b31370ca8f1fb649fbcbdd1d
miniwa <blarch3030@gmail.com>
2018-02-06 11:46:39 +0100

    Added thread-safety to Http::Client

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 source "https://rubygems.org" do
+    gem "concurrent-ruby"
     gem "rest-client"
     gem "rspec"
     gem "rspec-mocks"
--- lib/lygens/http/client.rb
+++ lib/lygens/http/client.rb
@@ -1,4 +1,5 @@
 # Holds code for the HTTP client and related things
+require "concurrent"
 require "rest-client"
 
 module Lygens
@@ -9,37 +10,44 @@ module Lygens
             # Initializes the +Client+ with a given +Transport+
             def initialize(transport)
                 @transport = transport
-                @headers = {}
-                @cookies = {}
-                @follow_redirects = false
+                @headers = Concurrent::Hash.new
+                @cookies = Concurrent::Hash.new
                 @autosave_cookies = false
             end
 
+            # Returns the preset headers of the client
             def headers
                 return @headers
             end
 
+            # Returns the preset cookies of the client
             def cookies
                 return @cookies
             end
 
+            # Makes a http request with the given parameters. The hash may
+            # contain the following values:
+            # * +url+ - The url to make the request to.
+            # * +method+ - The method to use when making the request.
+            # * +headers+ - A hash containing a set of headers.
+            # * +params+ - A hash containing a set of query parameters.
+            # * +payload+ - A hash containing a set of payloads.
             def make_request(params)
                 unless params.key?(:url) && params.key?(:method)
                     raise ArgumentError, "Method and url are required"
                 end
 
                 actual = params.clone
-                if params.key?(:headers)
-                    actual[:headers] = @headers.merge(params[:headers])
-                else
-                    actual[:headers] = @headers.clone
-                end
-
-                if params.key?(:cookies)
-                    actual[:cookies] = @cookies.merge(params[:cookies])
-                else
-                    actual[:cookies] = @cookies.clone
-                end
+                actual[:headers] =  if params.key?(:headers)
+                                        @headers.merge(params[:headers])
+                                    else
+                                        @headers.clone
+                                    end
+                actual[:cookies] =  if params.key?(:cookies)
+                                        @cookies.merge(params[:cookies])
+                                    else
+                                        @cookies.clone
+                                    end
                 response = @transport.make_request(actual)
 
                 if @autosave_cookies
