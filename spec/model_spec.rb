d6534af324487c46780513d1801fb63d1c9c7d62
miniwa <blarch3030@gmail.com>
2018-02-08 22:56:05 +0100

    Fixed warnings in model tests

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -3,7 +3,7 @@ require "lygens/model/model"
 RSpec.describe Lygens::Model do
     describe "#define" do
         it "should create a model with the proper fields" do
-            Person = Lygens::Model.define do
+            Person1 = Lygens::Model.define do
                 field :name do
                     required true
                 end
@@ -13,12 +13,12 @@ RSpec.describe Lygens::Model do
                 end
             end
 
-            name_field = Person.fields[0]
+            name_field = Person1.fields[0]
             expect(name_field.name).to eq(:name)
             expect(name_field.key).to eq(:name)
             expect(name_field.required).to eq(true)
 
-            age_field = Person.fields[1]
+            age_field = Person1.fields[1]
             expect(age_field.name).to eq(:age)
             expect(age_field.key).to eq(:years_old)
             expect(age_field.required).to eq(false)
@@ -26,12 +26,12 @@ RSpec.describe Lygens::Model do
 
         it "should have fields representing the definition"\
         " that are assignable" do
-            Person = Lygens::Model.define do
+            Person2 = Lygens::Model.define do
                 field :name do
                 end
             end
 
-            person = Person.new
+            person = Person2.new
             person.name = "test"
             expect(person.name).to eq("test")
         end
@@ -39,7 +39,7 @@ RSpec.describe Lygens::Model do
 
     describe "#parse" do
         it "should parse a hash with all values correctly" do
-            Person = Lygens::Model.define do
+            Person3 = Lygens::Model.define do
                 field :name do
                 end
             end
@@ -47,26 +47,26 @@ RSpec.describe Lygens::Model do
                 "name" => "Test"
             }
 
-            person = Person.parse(hash)
+            person = Person3.parse(hash)
             expect(person.name).to eq("Test")
         end
 
         context "when non-required field is missing value" do
             it "should equal nil" do
-                Person = Lygens::Model.define do
+                Person4 = Lygens::Model.define do
                     field :name do
                     end
                 end
                 hash = {}
 
-                person = Person.parse(hash)
+                person = Person4.parse(hash)
                 expect(person.name).to eq(nil)
             end
         end
 
         context "when required field is missing value" do
             it "should raise SerializationError" do
-                Person = Lygens::Model.define do
+                Person5 = Lygens::Model.define do
                     field :name do
                         required true
                     end
@@ -76,14 +76,14 @@ RSpec.describe Lygens::Model do
                     "asd" => "Test"
                 }
                 expect do
-                    Person.parse(hash)
+                    Person5.parse(hash)
                 end.to raise_error(Lygens::SerializationError)
             end
         end
 
         context "when field has a key different from its name" do
             it "should parse values from the key instead" do
-                Person = Lygens::Model.define do
+                Person6 = Lygens::Model.define do
                     field :name do
                         key :first_name
                     end
@@ -92,7 +92,7 @@ RSpec.describe Lygens::Model do
                     "first_name" => "Test"
                 }
 
-                person = Person.parse(hash)
+                person = Person6.parse(hash)
                 expect(person.name).to eq("Test")
             end
         end
